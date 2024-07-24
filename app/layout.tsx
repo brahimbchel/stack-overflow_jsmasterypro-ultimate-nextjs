b3e4ccba3f7a3ec3302d8633841edf28aff53c9c
brahimbchel <brahimbchel@gmail.com>
2024-07-24 13:03:45 +0100

    add layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,29 @@
-
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import './globals.css'
 import React from 'react'
+import { ClerkProvider } from '@clerk/nextjs'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import type { Metadata } from 'next';
+
+import './globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+})
+
+export const metadata: Metadata = {
+  title: 'BmDevFlow',
+  description: 'A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
+}
 
 export default function RootLayout({
   children,
@@ -15,15 +31,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500'
+        }
+      }}
+    >
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           {children}
         </body>
       </html>
@@ -31,6 +48,45 @@ export default function RootLayout({
   )
 }
 
+
+// ----------------------------------------------------------------
+
+
+// import {
+//   ClerkProvider,
+//   SignInButton,
+//   SignedIn,
+//   SignedOut,
+//   UserButton
+// } from '@clerk/nextjs'
+// import './globals.css'
+// import React from 'react'
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <ClerkProvider>
+//       <html lang="en">
+//         <body>
+//           <SignedOut>
+//             <SignInButton />
+//           </SignedOut>
+//           <SignedIn>
+//             <UserButton />
+//           </SignedIn>
+//           {children}
+//         </body>
+//       </html>
+//     </ClerkProvider>
+//   )
+// }
+
+
+
+// ----------------------------------------------------------------------
 
 
 
