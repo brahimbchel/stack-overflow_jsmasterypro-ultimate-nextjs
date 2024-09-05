7c94029ea3a81f3d32ecb018974cba616df0c87b
brahimbchel <brahimbchel@gmail.com>
2024-09-05 10:38:03 +0100

    add user img in qst card ...

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -67,7 +67,6 @@ const QuestionCard = ({
 
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
-          // imgUrl="/assets/icons/avatar.svg"
           imgUrl={author.picture}
           alt="user"
           value={author.name}
